04d0810485843f2ee5b9cef7b44b61bd66371544
han <hanocator@gmail.com>
2021-02-18 21:02:08 +0900

    test 임시삭제

---
+++ src/main/java/com/han/springboot/web/IndexController.java
@@ -0,0 +1,43 @@
+package com.han.springboot.web;
+
+import com.han.springboot.config.auth.LoginUser;
+import com.han.springboot.config.auth.dto.SessionUser;
+import com.han.springboot.service.posts.PostsService;
+import com.han.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user){
+        model.addAttribute("posts", postsService.findeAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        model.addAttribute("post", postsService.findById(id));
+        return "posts-update";
+    }
+}
